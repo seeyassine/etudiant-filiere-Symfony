4a471b1bc494d0efbdeaeac017cf3bf7c5b4c0ad
See Yassine <seeyassine@gmail.com>
2025-03-04 14:29:44 +0000

    update

---
+++ tests/Service/CalculatorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(2, 3);
+
+        $this->assertEquals(5, $result);
+    }
+}
